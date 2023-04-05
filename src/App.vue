6113e10cf6b1b1ea9ed1c07e87bdd7104c9666c7
Thiago <thiagomoutinho34@gmail.com>
2023-04-05 14:02:55 -0300

    vueex

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="livro in $store.state.livros" :key="livro">
+        {{ livro.nome }}
+      </li>
+    </ul>
+    {{ $store.state.acao }}
+   <Aluno /> 
+   <Curso />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Aluno from "@/components/Aluno.vue";
+import Curso from "@/components/Curso.vue"
+
+
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    Aluno,
+    Curso,
+  }, 
+
+  created() {
+    this.$store.dispatch("puxarAcao");
   }
 }
 </script>
@@ -21,7 +35,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Curso.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <div>
+        <p>Curso de JavaScript</p>
+        <Aulas />
+    </div>
+</template>
+<script>
+import Aulas from "@/components/Aulas.vue"
+
+export default {
+    name: "CursoVue",
+    components: {
+        Aulas,
+    }
+}
+</script>
+<style lang="">
+    
+</style>
